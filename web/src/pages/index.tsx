7ebbcb4a07d17f1f8029b057a4f12a49f47a721a
javedbartlett <javedbartlett@gmail.com>
2020-11-09 19:54:42 -0600

    renamed folders, added basic elements to register page

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@chakra-ui/core'
+import { DarkModeSwitch } from '../components/DarkModeSwitch'
+
+const Index = () => (
+  <div>
+    <Button>Hello</Button>
+    </div>
+    
+)
+
+export default Index
---
+++ web/src/pages/register.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Form, Formik } from "formik";
+import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/core';
+import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
+
+interface registerProps {
+
+}
+
+export const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <Box mt={4}>
+            <InputField
+              name="password"
+              placeholder="password"
+              label="Password"
+              type="password"
+            />
+            </Box>
+            <Button mt={4} type="submit" isLoading={isSubmitting} colorScheme="teal">Register</Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register
