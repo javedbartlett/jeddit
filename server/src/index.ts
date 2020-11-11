c5b8cefc40473281669c0e41a1bf75c4c1e4e11d
javedbartlett <javedbartlett@gmail.com>
2020-11-10 18:41:56 -0600

    added frontend register query

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,11 +7,10 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
-import redis from 'redis';
+import redis from "redis";
 import session from "express-session";
-import connectRedis from 'connect-redis';
-import { MyContext } from "./types";
-
+import connectRedis from "connect-redis";
+import cors from "cors";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -23,20 +22,27 @@ const main = async () => {
   const redisClient = redis.createClient();
 
   app.use(
+    cors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    })
+  );
+
+  app.use(
     session({
-      name: 'qid', // name of cookie
-      store: new RedisStore({ 
-        client: redisClient,  // telling express-session we're using redis
+      name: "yep", // name of cookie
+      store: new RedisStore({
+        client: redisClient, // telling express-session we're using redis
         disableTouch: true, // reduces number of requests to redis, stops resaving.
-       }),
-       cookie: {
-         maxAge: 315569520000, // 10 years
-         httpOnly: true, // cannot access cookie in JS frontend code
-         sameSite: 'lax', // csrf
-         secure: __prod__ // cookie only works in https
-       },
+      }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true, // cannot access cookie in JS frontend code
+        sameSite: "lax", // csrf
+        secure: __prod__, // cookie only works in https
+      },
       saveUninitialized: false, // stops storing of data if nothing is created in the session
-      secret: "adsadsadsadadsad", // how we're signing the cookie, should make this env variable
+      secret: "dasdasasdadsadd", // how we're signing the cookie, should make this env variable
       resave: false, // stops continuous pings to redis
     })
   );
@@ -46,16 +52,18 @@ const main = async () => {
       resolvers: [PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({req, res}): MyContext => ({ em: orm.em, req, res }),
+    context: ({ req, res }) => ({ em: orm.em, req, res }),
+
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({
+    app,
+    cors: false,
+  });
 
   app.listen(4000, () => {
-    console.log(`server started on 4000`)
-  })
+    console.log(`server started on 4000`);
+  });
+};
 
-}
-
-main()
-.catch(err => console.log(err));
+main().catch((err) => console.log(err));
--- web/src/pages/register.tsx
+++ web/src/pages/register.tsx
@@ -3,18 +3,27 @@ import { Form, Formik } from "formik";
 import { Box, Button, FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
 import { InputField } from '../components/InputField';
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import {useRouter} from 'next/router';
 
-interface registerProps {
-
-}
+interface registerProps {}
 
 export const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
+  const [{},register] = useRegisterMutation();
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          console.log(values);
+        onSubmit={async (values, {setErrors}) => {
+          const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            // it worked
+            router.push("/");
+          }
         }}
       >
         {({ isSubmitting }) => (
